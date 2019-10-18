e576f6fa3356955e9a9807558379d205e67c33a0
Your Name <you@example.com>
2019-10-18 06:42:06 -0300

    Aula javascript

--- script.js
+++ script.js
@@ -5,28 +5,32 @@ Script para configuração do slide da Celula de Desenvolvimento Hetero
 */
 
 let indice = 0;
-let n = 6;
+let n = 6; // Número de Slides
 let slides = [];
 
 function next() { // Vai para o proximo slide
-	document.getElementById("slide_"+indice).setAttribute('style', "visibility: hidden")
-	indice = indice + 1;
-	loading_slide();
+	if (indice < n) {
+		indice = indice + 1;
+		loading_slide();
+	}
 }
 
 function before() { // Vai para o slide anterior
-	document.getElementById("slide_"+indice).setAttribute('style', "visibility: hidden")
-	indice = indice - 1;	
-	loading_slide();
+	if(indice > 0){
+		indice = indice - 1;	
+		loading_slide();
+	}
 }
 
-function loading_slide(){
-	let pagina_atual = document.getElementById('slide');
+function loading_slide(){ // carrega o conteudo atual
+	let pagina_atual = document.getElementById('indice');
 	pagina_atual.innerHTML = indice;
 	
-	// console.log("slide_1"+indice);
 	let texto_atual = document.getElementById("slide_"+indice);
+	let front = document.getElementById('front');
 
-	texto_atual.setAttribute('style', "visibility: visible");
-	// texto_atual.set
+	front.innerHTML = texto_atual.innerHTML;
+
+
+
 }
