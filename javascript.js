27d5b94b0f26ad994bfac70139f30cd460e52250
Wesley Santos <fwsantos23@gmail.com>
2019-10-18 13:36:59 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,32 @@
+let sendoh = ["3", 1.92];
+alert();
+
+
+
+let mudarnome = function(){
+	let titulo = document.getElementById("titulo");
+
+	if(titulo.innerHTML == "CHICO WESLEY"){
+		titulo.innerHTML = "BLADE BAIANO";
+	}else{
+		titulo.innerHTML = "CHICO WESLEY";
+	}
+
+}
+
+let cocegas = function(){
+	let img = document.getElementById('img');
+
+	img.src = "sorrindo.jpeg";
+}
+
+let abandonar = function(){
+	let img = document.getElementById('img');
+
+	img.src = "ines.jpeg";	
+}
+let espan = function(){
+	let img = document.getElementById('img');
+
+	img.src = "espan.jpeg";	
+}
