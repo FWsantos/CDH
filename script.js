011ecadfdf3f86b16e818204f4e911fd247860e1
FWSantos <fwsantos23@gmail.com>
2019-11-08 13:12:25 -0300

    teste

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let checar = function() {
 	let nome = document.getElementById('nome').value;
-	alert(nome);
+	// alert(nome);
+	let senha = document.querySelector("#senha");
+	if (nome == "") {
+		senha.setAttribute('disabled', '');
+	}else{
+		senha.removeAttribute('disabled');
+	}
+	// alert(senha);
+
 }
